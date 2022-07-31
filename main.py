3fc590c8248e5df7f954a568be455a1660f7ecd2
Amaze <100564184+AmazeHere@users.noreply.github.com>
2022-07-31 10:59:58 +0530

    main.py file added

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = ">")
+
+
+@client.event
+async def on_ready():
+    print("this sexy bot has been connected to discord")
+async def on_message(message):
+    print(f'\033[1;36;40m {message.author}: {message.content}') #this line print the whole chat cuz ez message logger heheha
+    if message.author == client.user:
+        return
+
+@client.command()
+async def ping(ctx):
+     await ctx.send(f'your ping is {round(client.latency * 1000)}ms')
+
+client.run("OTk0NjIxMjg3Mjg2NzE4NTA0.GbjuYO.CoJJ0Hg_i8LqBb9NQHsocwiJOM2YrKwv0r3dNk")
