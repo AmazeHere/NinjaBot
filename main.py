afa6ee868ceac75a79a8f5e33597bf213205b1a6
Amaze <100564184+AmazeHere@users.noreply.github.com>
2022-07-31 18:46:13 +0530

    rewrite @rasp pls fix

--- main.py
+++ main.py
@@ -12,17 +12,15 @@ client = commands.Bot(command_prefix = ">")
 @client.event
 async def on_ready():
     print("this sexy bot has been connected to discord")
+    
+@client.event
 async def on_message(message):
     print(f'\033[1;36;40m {message.author}: {message.content}') #this line print the whole chat cuz ez message logger heheha
     if message.author == client.user:
         return
 
-@client.command()
+@Bot.command
 async def ping(ctx):
-     await ctx.send(f'your ping is {round(client.latency * 1000)}ms')
-     
-@bot.command()
-async def say(ctx, message=None)
-    await ctx.send(message)
+     await ctx.send(f'your ping is {round(client.latency)}ms')
 
-client.run("OTk0NjIxMjg3Mjg2NzE4NTA0.GbjuYO.CoJJ0Hg_i8LqBb9NQHsocwiJOM2YrKwv0r3dNk")
+client.run("OTk0NjIxMjg3Mjg2NzE4NTA0.G5iJbP.bSWeqPsVkevDttpOUEbvakLygrsukxcpsVhuzY")
