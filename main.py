bba8424da55063d658ca567d6a4944fcadf3b4c4
namooTH <73419369+namooTH@users.noreply.github.com>
2022-07-31 20:49:09 +0700

    fix ur stupid import bot and forgoring to add the command

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
 
-
-Client = discord.Client()
 client = commands.Bot(command_prefix = ">")
 
+cookie = '\N{COOKIE}'
 
 @client.event
 async def on_ready():
@@ -19,8 +17,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('cookies'):
+        await message.channel.send(cookie)
+
+    await client.process_commands(message)
+
 @client.command
 async def ping(ctx):
      await ctx.send(f'your ping is {round(client.latency)}ms')
 
+client.add_command(ping)
 client.run("OTk0NjIxMjg3Mjg2NzE4NTA0.G5iJbP.bSWeqPsVkevDttpOUEbvakLygrsukxcpsVhuzY")
