0828c02b92a10624fb76b2adb80ff440261b18d9
Plextora <plextoraa.a@gmail.com>
2022-08-13 17:09:17 -0400

    Transfer the big boi shit to responses.py

--- main.py
+++ main.py
@@ -20,34 +20,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    await responses.cookie(message)
-
-    if message.content.startswith('free'):
-        await message.channel.send('<https://youtu.be/xvFZjo5PgG0>')
-
-    if message.content.startswith('coc'):
-        await message.channel.send('🐔')
-
-    if message.content.startswith('77+33'):
-        await message.channel.send('100')
-
-    if message.content.startswith('33+77'):
-        await message.channel.send('100')
-
-    if message.content.startswith('goofy'):
-        await message.channel.send('ahh')
-
-    if 'kiss' in message.content:
-        print(f'react to {message.author}')
-        await message.add_reaction("😽")
-
-    if 'dick' in message.content:
-        print(f'react to {message.author}')
-        await message.add_reaction("🍆")
-
-    if 'onix' in message.content:
-        print(f'react to {message.auther}')
-        await message.add_reaction("🤡")
+    await responses.responses(message)
 
 
 @bot.command()
--- responses.py
+++ responses.py
@@ -1,3 +1,30 @@
-async def cookie(message):
+async def responses(message):
     if message.content.startswith('cookie'):
         await message.channel.send("🍪")
+
+    if message.content.startswith('free'):
+        await message.channel.send('<https://youtu.be/xvFZjo5PgG0>')
+
+    if message.content.startswith('coc'):
+        await message.channel.send('🐔')
+
+    if message.content.startswith('77+33'):
+        await message.channel.send('100')
+
+    if message.content.startswith('33+77'):
+        await message.channel.send('100')
+
+    if message.content.startswith('goofy'):
+        await message.channel.send('ahh')
+
+    if 'kiss' in message.content:
+        print(f'react to {message.author}')
+        await message.add_reaction("😽")
+
+    if 'dick' in message.content:
+        print(f'react to {message.author}')
+        await message.add_reaction("🍆")
+
+    if 'onix' in message.content:
+        print(f'react to {message.auther}')
+        await message.add_reaction("🤡")
