02692364e38d1c44b7e14bfc37ed68b78b338673
Amaze <100564184+AmazeHere@users.noreply.github.com>
2022-08-29 22:18:54 +0530

    bruh

--- main.py
+++ main.py
@@ -1,49 +1,52 @@
-import asyncio
-import json
+import discord
+from discord.ext import commands
+import responses
 import os
 import sys
-import traceback
-import discord 
-from discord.ext import commands
+import time
+import threading, time
 
-class Bot(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    async def setup_hook(self):
-        pass
-    
-bot = Bot(
-    command_prefix=">",
-    intents=discord.Intents().all(),
-    owner_ids=[900793535828197446, 875213353658777620, 741486101218197565, 958390293760184392],
-    case_insensitive=True,
-    status=discord.Status.dnd,
-    activity=discord.Activity(type=discord.ActivityType.watching, name="Onix play with himself cuz lonely")
-)
+# i use 2.0 so intents are needed
+bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
+bot.owner_ids = [900793535828197446, 875213353658777620, 741486101218197565]
 
-async def main():
-    for cog in os.listdir("./cogs"):
-        try:
-            await bot.load_extension("cogs."+cog[:-3])
-        except Exception as e:
-            print(f"Error Loading {cog}", file=sys.stderr)
-            traceback.print_exc()
-        # await bot.load_extension("jishaku")
-    print("Logging in...")
-    await bot.load_extension('jishaku')
-    with open("config.json", "r") as f:
-        data = json.load(f)
-    async with bot:
-        await bot.start(data['TOKEN'])
 
 @bot.event
 async def on_ready():
-    print("======================================")
-    print(f"Logged in")
-    print(f"Bot Name: {bot.user}")
-    print(f"Bot ID: {bot.user.id}")
-    print("======================================")
+    print('bot has been connected to discord :D')
 
 
-asyncio.run(main())
+@bot.listen()
+async def on_message(message):
+    print(f'\033[1;36;40m {message.author}: {message.content}')
+    if message.author == bot.user:
+        return
+
+    # one line for all responses in main.py!
+    await responses.responses(message)
+
+@bot.command()
+@commands.is_owner()
+async def restart(ctx):
+    await ctx.send("restarting bot :D")
+    os.execv(sys.executable, ["python"]+sys.argv)
+
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.send("bot has been shutdown")
+    await ctx.bot.close()
+
+@bot.command()
+async def ping(ctx):
+    start=time.perf_counter()
+    msg = await ctx.send("Latency: ...ms\nWebsocket: ...ms")
+    end=time.perf_counter()
+    duration=(end-start)*1000
+    await msg.edit(content=f"Latency: {duration:.2f}ms\nWebsocket: {bot.latency*1000:.2f}ms")
+    
+@bot.command()
+async def say(ctx, message=None):
+    await ctx.send(message)
+    
+bot.run("OTk0NjIxMjg3Mjg2NzE4NTA0.G5iJbP.bSWeqPsVkevDttpOUEbvakLygrsukxcpsVhuzY")
